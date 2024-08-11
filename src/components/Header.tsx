8d7d985fdead01aa85f8613976130e8aac034c1b
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-08-11 16:56:21 +0600

    Header comments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import { Box } from "@mui/material";
 import SearchBar from './SearchBar';
 
+/**
+ * Компонент Header отображает верхнюю часть страницы, включая панель поиска.
+ */
 const Header: React.FC<{ onSearch: () => void }> = ({ onSearch }) => {
     return (
-        <Box sx={{ height: '80px', backgroundColor: 'rgba(79, 79, 79, 1)', padding: 0, marginLeft: '-25px', width: '100%' }}>
+        <Box
+            sx={{
+                height: '80px',
+                backgroundColor: 'rgba(79, 79, 79, 1)',
+                padding: 0,
+                marginLeft: '-25px',
+                width: '100%'
+            }}
+        >
+            {/* Компонент панели поиска, который использует переданную функцию onSearch */}
             <SearchBar onSearch={onSearch} />
         </Box>
     );
