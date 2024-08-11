9bdd044fb4b7efb9486d9b90da19df4bcef8c1bf
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-08-11 17:14:38 +0600

    SearchBar comments

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,27 +1,49 @@
 import React, { useState } from 'react';
-import {TextField, Button, Box} from '@mui/material';
+import { TextField, Button, Box } from '@mui/material';
 import { useAppDispatch } from '../redux/store';
 import { fetchRepos, setSearchQuery, resetRepos } from '../redux/reposSlice';
 import styles from '../styles/SearchBar.module.scss';
 
-
+/**
+ * Интерфейс для пропсов компонента SearchBar.
+ *
+ * @property {() => void} onSearch - Функция, вызываемая для выполнения поиска.
+ * @property {string} [width] - Опциональная ширина компонента, по умолчанию 1480px.
+ */
 interface SearchBarProps {
     onSearch: () => void;
-    width?: string; // Добавляем width
+    width?: string;
 }
 
+/**
+ * Компонент SearchBar отображает строку поиска с текстовым полем и кнопкой для выполнения поиска.
+ *
+ * @param {SearchBarProps} props - Пропсы, содержащие функцию onSearch и опциональную ширину компонента.
+ * @returns {JSX.Element} - Возвращает JSX элемент строки поиска.
+ */
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch, width }) => {
+    // Локальное состояние для хранения текущего поискового запроса
     const [query, setQuery] = useState('');
+
+    // Хук для доступа к функции dispatch из Redux
     const dispatch = useAppDispatch();
 
+    /**
+     * Функция handleSearch выполняет обработку поиска.
+     * Если запрос пуст, сбрасывает результаты поиска.
+     * В противном случае отправляет запрос на поиск репозиториев.
+     */
     const handleSearch = () => {
         if (query.trim() === '') {
+            // Если строка поиска пустая, сбрасываем результаты поиска
             dispatch(resetRepos());
         } else {
-            dispatch(setSearchQuery(query)); // Устанавливаем значение query в store
-            // первый запрос выполняется без сортировки (используется best match по умолчанию)
+            // Устанавливаем значение поискового запроса в store
+            dispatch(setSearchQuery(query));
+            // Выполняем запрос на получение репозиториев
             dispatch(fetchRepos({ query, sort: undefined, direction: undefined, page: 0, rowsPerPage: 10 }));
         }
+        // Вызываем переданную функцию onSearch
         onSearch();
     };
 
@@ -39,7 +61,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch, width }) => {
                 inputProps={{
                     className: styles.inputBase,
                 }}
-                InputProps={{ style: { height: '42px'} }}
+                InputProps={{ style: { height: '42px' } }}
             />
             <Button
                 variant="contained"
